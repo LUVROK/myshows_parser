e828ef4f2c24b849aa196f1a56bb929139a08781
Pawel Proger <pawelmyhrygin28@gmail.com>
2022-12-01 23:32:49 +0600

    0.1.9

--- index.js
+++ index.js
@@ -20,21 +20,31 @@ const ObjectEntrize = {
   counter: 0,
   data: [],
   rndEl: 0,
+  jquerySearch: "",
+  jqueryA: "",
+  jqueryLeft: "",
+  jqueryRight: "",
+  intervalId: "",
 };
-
-let intervalId;
+let site;
 
 function loading() {
   var h = ["processing.", "processing..", "processing..."];
   var i = 0;
 
-  intervalId = setInterval(() => {
+  ObjectEntrize.intervalId = setInterval(() => {
     i = i > 2 ? 0 : i;
     readline.moveCursor(process.stdout, 0, -1);
     readline.clearScreenDown(process.stdout);
     console.log(h[i]);
     i++;
   }, 300);
+}
+
+function wait(ms = 0) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
 const parse = async () => {
@@ -47,62 +57,142 @@ const parse = async () => {
     }
   };
 
-  rl.question(`Choose one thing what data do you want to get:\n1 - Watching\n2 - Going to watch\n3 - Stopped watching\n4 - Watched all\n5 - Everything\n`, function (opt_1) {
-    opt_1++;
-    readline.moveCursor(process.stdout, 0, -7);
+  rl.question(`Choose which version of myshows to parse:\n1 - old version\n2 - new version\n3 - exit\n`, function (opt_version) {
+    readline.moveCursor(process.stdout, 0, -5);
     readline.clearScreenDown(process.stdout);
-    opt_1 < 2 || opt_1 > 7
-      ? (console.log("Invalid number"), rl.close())
-      : rl.question(`Enter your nickname:\n`, async function (opt_2) {
-          readline.moveCursor(process.stdout, 0, -2);
-          readline.clearScreenDown(process.stdout);
-          loading();
-          console.log("processing");
-          const $ = await getHTML(`https://old.myshows.me/${opt_2.toString()}?fl=en`);
-          clearInterval(intervalId);
-          readline.moveCursor(process.stdout, 0, -1);
-          readline.clearScreenDown(process.stdout);
-          $ === undefined || $ === null
-            ? (console.log("Incorrect nickname"), rl.close())
-            : rl.question(`What do you want to do:\n1 - Get random movie\n2 - Get a convenient table\n`, async function (opt_3) {
-                readline.moveCursor(process.stdout, 0, -4);
-                readline.clearScreenDown(process.stdout);
-                function getMovies() {
-                  let _done_mass = [];
-                  let _left_mass = [];
-                  $(`.tabs_cont:nth-child(${opt_1}) .showProgress ._left`).each((i, element) => {
-                    _left_mass.push($(element).text());
-                  });
-                  $(`.tabs_cont:nth-child(${opt_1}) .showProgress ._done`).each((i, element) => {
-                    _done_mass.push($(element).text());
-                  });
-                  $(`.tabs_cont:nth-child(${opt_1}) td a`).each((i, element) => {
-                    if (!$(element).text().includes("Show others")) {
-                      ObjectEntrize.data.push({ title: $(element).text(), Episodes_watched_done: _done_mass[i], Episodes_watched_left: _left_mass[i] });
-                      ObjectEntrize.counter++;
-                    }
-                  });
-                }
-                switch (opt_3) {
-                  case "1":
-                    await getMovies();
-                    break;
-                  case "2":
-                    await getMovies();
-                    console.log(ObjectEntrize.data);
-                    console.log("your choose is 2"), rl.close();
-                    break;
-                  default:
-                    console.log("Invalid number"), rl.close();
-                    break;
-                }
-                opt_3 === "1" ? ((ObjectEntrize.rndEl = await getRandomArbitrary(0, ObjectEntrize.data.length)), console.log(`\nRandom movie`), console.log(`----------------------------------`), console.log(`Index - ${ObjectEntrize.rndEl}`), console.log(`----------------------------------`), console.log(`Title - ${ObjectEntrize.data[ObjectEntrize.rndEl].title}`)) : null;
-                console.log(`----------------------------------\n`);
-                console.log(`----------------------------------\nTotal movies - ${ObjectEntrize.counter}`);
-                console.log(`----------------------------------\n`);
-                rl.close();
-              });
-        });
+    switch (opt_version) {
+      case "1":
+        site = `old.`;
+        ObjectEntrize.jquerySearch = ".tabs_cont";
+        ObjectEntrize.jqueryA = "td a";
+        ObjectEntrize.jqueryLeft = ".showProgress ._left";
+        ObjectEntrize.jqueryRight = ".showProgress ._done";
+        break;
+      case "2":
+        site = ``;
+        ObjectEntrize.jquerySearch = ".UserShowItem-header__top";
+        ObjectEntrize.jqueryA = ".UserShowItem-title";
+        ObjectEntrize.jqueryLeft = ".UserShowItem-watched";
+        ObjectEntrize.jqueryRight = ".Progress-secondary:last-of-child";
+        break;
+      case "3":
+        console.log("exit"), rl.close();
+        break;
+      default:
+        console.log("Invalid number"), rl.close();
+        break;
+    }
+    rl.question(`Choose one thing what data do you want to get:\n1 - Watching\n2 - Going to watch\n3 - Stopped watching\n4 - Watched all\n5 - Everything\n6 - exit\n`, function (opt_1) {
+      let numStr = 7;
+      opt_1++;
+      opt_1 === 6 ? (opt_1 = ``) : opt_1 === 7 ? (opt_1 = 7) : ((opt_1 = `:nth-child(${opt_1})`), (numStr = `:nth-child(${opt_1})`));
+      readline.moveCursor(process.stdout, 0, -8);
+      readline.clearScreenDown(process.stdout);
+      opt_1 !== "" && opt_1 === numStr && opt_1 === 7
+        ? opt_1 === 7
+          ? (console.log("exit"), rl.close())
+          : (console.log("Invalid number"), rl.close())
+        : rl.question(`Enter your nickname:\n`, async function (opt_2) {
+            readline.moveCursor(process.stdout, 0, -2);
+            readline.clearScreenDown(process.stdout);
+            loading();
+            console.log("processing");
+            const $ = await getHTML(`https://${site}myshows.me/${opt_2.toString()}?fl=en`);
+            clearInterval(ObjectEntrize.intervalId);
+            readline.moveCursor(process.stdout, 0, -1);
+            readline.clearScreenDown(process.stdout);
+            $ === undefined || $ === null
+              ? (console.log("Incorrect nickname"), rl.close())
+              : rl.question(`What do you want to do:\n1 - Get random movie\n2 - Get a convenient table\n3 - exit\n`, async function (opt_3) {
+                  readline.moveCursor(process.stdout, 0, -5);
+                  readline.clearScreenDown(process.stdout);
+
+                  switch (opt_3) {
+                    case "1":
+                      loading();
+                      console.log("processing");
+
+                      await new Promise((resolve, reject) => {
+                        setTimeout(function () {
+                          let _done_mass = [];
+                          let _left_mass = [];
+
+                          const LASTELEMENT = $(`${ObjectEntrize.jquerySearch}${opt_1} ${ObjectEntrize.jqueryA}`).length - 2;
+
+                          $(`${ObjectEntrize.jquerySearch}${opt_1} ${ObjectEntrize.jqueryLeft}`).each((i1, element) => {
+                            _left_mass.push($(element).text());
+                          });
+                          $(`${ObjectEntrize.jquerySearch}${opt_1} ${ObjectEntrize.jqueryRight}`).each((i1, element) => {
+                            _done_mass.push($(element).text());
+                          });
+                          $(`${ObjectEntrize.jquerySearch}${opt_1} ${ObjectEntrize.jqueryA}`).each((i, element) => {
+                            if (!$(element).text().includes("Show others")) {
+                              ObjectEntrize.data.push({ title: $(element).text(), Episodes_watched_done: _done_mass[i], Episodes_watched_left: _left_mass[i] });
+                              ObjectEntrize.counter++;
+                            }
+                            if (i === LASTELEMENT) {
+                              console.log(ObjectEntrize.counter);
+                              clearInterval(ObjectEntrize.intervalId);
+                              readline.moveCursor(process.stdout, 0, -2);
+                              readline.clearScreenDown(process.stdout);
+                              resolve("");
+                            }
+                          });
+                        }, 2000);
+                      });
+                      ObjectEntrize.rndEl = await getRandomArbitrary(0, ObjectEntrize.data.length);
+                      opt_3 === "1" ? (console.log(`\n----------------------------------\nRandom movie`), console.log(`----------------------------------`), console.log(`Index - ${ObjectEntrize.rndEl}`), console.log(`----------------------------------`), console.log(`Title - ${ObjectEntrize.data[ObjectEntrize.rndEl].title}`)) : null;
+                      console.log(`----------------------------------\n`);
+                      console.log(`----------------------------------\nTotal movies - ${ObjectEntrize.counter}`);
+                      console.log(`----------------------------------\n`);
+                      rl.close();
+                      break;
+                    case "2":
+                      loading();
+                      console.log("processing");
+
+                      await new Promise((resolve, reject) => {
+                        setTimeout(function () {
+                          let _done_mass = [];
+                          let _left_mass = [];
+
+                          const LASTELEMENT = $(`${ObjectEntrize.jquerySearch}${opt_1} ${ObjectEntrize.jqueryA}`).length - 2;
+
+                          $(`${ObjectEntrize.jquerySearch}${opt_1} ${ObjectEntrize.jqueryLeft}`).each((i1, element) => {
+                            _left_mass.push($(element).text());
+                          });
+                          $(`${ObjectEntrize.jquerySearch}${opt_1} ${ObjectEntrize.jqueryRight}`).each((i1, element) => {
+                            _done_mass.push($(element).text());
+                          });
+                          $(`${ObjectEntrize.jquerySearch}${opt_1} ${ObjectEntrize.jqueryA}`).each((i, element) => {
+                            if (!$(element).text().includes("Show others")) {
+                              ObjectEntrize.data.push({ title: $(element).text(), Episodes_watched_done: _done_mass[i], Episodes_watched_left: _left_mass[i] });
+                              ObjectEntrize.counter++;
+                            }
+                            if (i === LASTELEMENT) {
+                              console.log(ObjectEntrize.counter);
+                              clearInterval(ObjectEntrize.intervalId);
+                              readline.moveCursor(process.stdout, 0, -2);
+                              readline.clearScreenDown(process.stdout);
+                              resolve("");
+                            }
+                          });
+                        }, 2000);
+                      });
+
+                      console.log(ObjectEntrize.data);
+                      console.log("your choose is 2"), rl.close();
+                      break;
+                    case "3":
+                      console.log("exit"), rl.close();
+                      break;
+                    default:
+                      console.log("Invalid number"), rl.close();
+                      break;
+                  }
+                });
+          });
+    });
   });
 };
 
